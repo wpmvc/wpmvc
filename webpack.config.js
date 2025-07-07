41ebfee5f17bbb5c0b4f09312402d34ff85536d9
mdalaminbey <mdalaminbey@gmail.com>
2025-07-07 11:35:59 +0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
-const devHost = 'wpmvc.test';
+const devHost = 'wpmvc.local';
 
 module.exports = {
 	...defaultConfig,
